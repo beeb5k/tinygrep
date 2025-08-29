5a542693ef80b7ddde11a2634b442afb20335d0a
beeb5k <beeb5k6@gmail.com>
2025-08-29 23:19:45 +0530

    bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     flake-utils.url = "github:numtide/flake-utils";
     fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -14,21 +16,31 @@
       nixpkgs,
       flake-utils,
       fenix,
+      naersk,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
         fenixLib = fenix.packages.${system};
         rustToolChain = fenixLib.stable.toolchain;
+        naerskLib = pkgs.callPackage naersk { };
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        version = cargoToml.package.version;
+        myPackage = naerskLib.buildPackage {
+          pname = "tgrep";
+          version = version;
+          edition = "2024";
+          src = ./.;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [ rustToolChain ];
         };
+
+        packages.default = myPackage;
       }
     );
 }
